620907a47d5e956b664b679b7f0d0ebe1f580f29
philippearnaud <philippe.demangou@gmail.com>
2014-08-25 13:46:07 +0200

    intégration du pingouin et de la bouteille de champagne

--- script.js
+++ script.js
@@ -1,59 +1,64 @@
 
 // Création d'une fonction add appelé à la fin du chargement de la page.
-	//Déclaration et instantiation d'un tableau pour stocker les valeurs de nos listes.
-	var liste = [];
+//Déclaration et instantiation d'un tableau pour stocker les valeurs de nos listes.
+var liste = [];
 
 function pick_a_winner() {
-	function hasard() {
-	    // Il faut qu'on récupère la taille du tableau de liste
-	   	 var choice = Math.floor(Math.random() * liste.length+1);
-	    console.log(choice);
-	    return choice;
-	};
-	
-	$('#button').click(function(){
-			$('ul li').removeClass('red');
-		$('ul li:nth-child('+ hasard() + ')').addClass('red').text('WINNER');
-	});
-};
+    function hasard() {
+        // Il faut qu'on récupère la taille du tableau de liste
+        var choice = Math.floor(Math.random() * liste.length);
+        console.log(choice);
+        return choice;
+    };
+        var compteur = false;
+        $('#button').click(ev){
+            if ( !compteur ) {
+            $('ul li').removeClass('red');
+            $('ul li:nth-child('+ hasard() + ')').addClass('red').text('WINNER');
+                compteur = true;
+                } else {
+                    ev.preventDefault();
+                }
+                }
+             }
 
-function add_list() { 
-	//Déclaration et instantiation d'une variable qui récupère l'élément du DOM sur lequel on va cliquer pour ajouter des éléments à la liste.
-	var add = document.getElementById('add');
-	//Déclaration et instantiationn d'une variable qui récupère l'élément du DOM dans lequel on va afficher nos listes.
-	var liste_appended = document.getElementById('ul-liste');
-	//Déclare une fonction qui va se déclencher dès qu'on cliquera sur l'élément du DOM stocké dans la variable add.
-	add.onclick = function() {
-		//Déclaration et instantiation d'une variable value dans lequel on va stocker la valeur qu'on a écrit dans le formulaire.
-		var value = document.forms['liste'].liste_add.value;
-		//On ajoute à la fin du tableau la valeur de la variable value.
-		liste.push(value);
-		//Déclaration et instantation d'une variable remove qui va nous permettre de virer un élément de la liste sur simple click.
-		var remove = document.createElement('span');
-		remove.className="remover";
-  	remove.onclick = function(ev) {
-  		this.parentNode.parentNode.removeChild(this.parentNode);
-  		console.log(ev);
-  	};
-		var remove_content = document.createTextNode('remove');
-		//Déclaration et instantiation d'une variable newLi dans laquelle on va stocker l'élément li du DOM qu'on vient de créer.
-		var newLi = document.createElement('li');	
-		// Déclaration et instantiation d'une variable liste_content dans laquelle on va stocker un noeud de texte correspondant à la variable value. 
-		var liste_content = document.createTextNode(value);
-		//On utilise AppendChild sur la variable newLi pour rajouter le texte à notre variable li. => <li> Texte </li>
-		newLi.appendChild(liste_content);
-		// On met le contenu remove dans l'élément span du dom
-		remove.appendChild(remove_content);
-		//On rajoute l'élément span dans la liste.
-		newLi.appendChild(remove);
-		//On utilise AppendChild sur la variable liste_appended pour rajouter à l'élement du DOM ul notre <li> Texte </li>
-		liste_appended.appendChild(newLi);
-	}
-}
+            function add_list() { 
+                //Déclaration et instantiation d'une variable qui récupère l'élément du DOM sur lequel on va cliquer pour ajouter des éléments à la liste.
+                var add = document.getElementById('add');
+                //Déclaration et instantiationn d'une variable qui récupère l'élément du DOM dans lequel on va afficher nos listes.
+                var liste_appended = document.getElementById('ul-liste');
+                //Déclare une fonction qui va se déclencher dès qu'on cliquera sur l'élément du DOM stocké dans la variable add.
+                add.onclick = function() {
+                    //Déclaration et instantiation d'une variable value dans lequel on va stocker la valeur qu'on a écrit dans le formulaire.
+                    var value = document.forms['liste'].liste_add.value;
+                    //On ajoute à la fin du tableau la valeur de la variable value.
+                    liste.push(value);
+                    //Déclaration et instantation d'une variable remove qui va nous permettre de virer un élément de la liste sur simple click.
+                    var remove = document.createElement('span');
+                    remove.className="remover";
+                    remove.onclick = function(ev) {
+                        this.parentNode.parentNode.removeChild(this.parentNode);
+                        console.log(ev);
+                    };
+                    var remove_content = document.createTextNode('remove');
+                    //Déclaration et instantiation d'une variable newLi dans laquelle on va stocker l'élément li du DOM qu'on vient de créer.
+                    var newLi =i document.createElement('li');	
+                    // Déclaration et instantiation d'une variable liste_content dans laquelle on va stocker un noeud de texte correspondant à la variable value. 
+                    var liste_content = document.createTextNode(value);
+                    //On utilise AppendChild sur la variable newLi pour rajouter le texte à notre variable li. => <li> Texte </li>
+                    newLi.appendChild(liste_content);
+                    // On met le contenu remove dans l'élément span du dom
+                    remove.appendChild(remove_content);
+                    //On rajoute l'élément span dans la liste.
+                    newLi.appendChild(remove);
+                    //On utilise AppendChild sur la variable liste_appended pour rajouter à l'élement du DOM ul notre <li> Texte </li>
+                    liste_appended.appendChild(newLi);
+                }
+            }
 
 function main() {
-	add_list();
-	pick_a_winner();
+    add_list();
+    pick_a_winner();
 }
 
 window.onload = main;
